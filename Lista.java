2b6504d119399d8af9403de9c268fe8d277cad90
johnatans2 <johnatan.henrique14@hotmail.com>
2026-01-30 06:59:17 -0300

    Adicionando sobrecarga de métodos na classe Lista para que não haja problemas na adição de elementos do tipo primitivo double.

--- Lista.java
+++ Lista.java
@@ -1,4 +1,12 @@
 public class Lista{
+	public static double[] adicionarElemento(double[] arrayOriginal,double elemento){
+		double[] novoArray=new double[arrayOriginal.length+1];
+		for(int iterador=0;iterador<arrayOriginal.length;iterador++){
+			novoArray[iterador]=arrayOriginal[iterador];
+		}
+		novoArray[arrayOriginal.length]=elemento;
+		return novoArray;
+	}
 	public static <T> T[] adicionarElemento(T[] arrayOriginal,T elemento){
 		@SuppressWarnings("unchecked")
 		T[] novoArray=(T[]) new Object[arrayOriginal.length+1];
