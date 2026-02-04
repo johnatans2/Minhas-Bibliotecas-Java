f3519b33cebd26c0d0d31031fee84762a0110b71
johnatans2 <johnatan.henrique14@hotmail.com>
2026-02-04 05:38:20 -0300

    Adicionando métodos de verificação de existência de elemento em um array; verificar o índice em que determinado elemento aparece em uma lista, e isso mesmo se o elemento se repetir na lista; adicionei verificações a métodos já existentes para diminuir a quantidade de processamento caso a existência de determinado elemento no array seja falsa; e também adicionei um método para adicionar elemento em um array de int, onde, anteriormente, eu somente havia criado para tipos double e de referência.

--- Lista.java
+++ Lista.java
@@ -7,6 +7,14 @@ public class Lista{
 		novoArray[arrayOriginal.length]=elemento;
 		return novoArray;
 	}
+	public static int[] adicionarElemento(int[] arrayOriginal,int elemento){
+		int[] novoArray=new int[arrayOriginal.length+1];
+		for(int iterador=0;iterador<arrayOriginal.length;iterador++){
+			novoArray[iterador]=arrayOriginal[iterador];
+		}
+		novoArray[arrayOriginal.length]=elemento;
+		return novoArray;
+	}
 	public static <T> T[] adicionarElemento(T[] arrayOriginal,T elemento){
 		@SuppressWarnings("unchecked")
 		T[] novoArray=(T[]) new Object[arrayOriginal.length+1];
@@ -15,5 +23,39 @@ public class Lista{
 		}
 		novoArray[arrayOriginal.length]=elemento;
 		return novoArray;
+	}
+
+	public static boolean verificarExistenciaElemento(double[] array,double elemento){
+		boolean resultado=false;
+		for(int iterador=0;iterador<array.length;iterador++){
+			if(array[iterador]==elemento){
+				resultado=true;
+			}
+		}
+		return resultado;
+	}
+
+	public static int verificarQuantidadeElemento(double[] array,double elemento){
+		int contador=0;
+		if(verificarExistenciaElemento(array,elemento)){
+			for(int iterador=0;iterador<array.length;iterador++){
+				if(array[iterador]==elemento){
+					contador++;
+				}
+			}
+		}
+		return contador;
+	}
+
+	public static int[] verificarIndiceElemento(double[] array,double elemento){
+		int[] arrayIndice=new int[0];
+		if(verificarExistenciaElemento(array,elemento)){
+			for(int iterador=0;iterador<array.length;iterador++){
+				if(array[iterador]==elemento){
+					arrayIndice=adicionarElemento(arrayIndice,iterador);
+				}
+			}
+		}
+		return arrayIndice;
 	}
 }
